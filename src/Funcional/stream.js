41e8577c3c1398e278ffa9560304095281fb3786
Gilson Santos <gilsonjosert@gmail.com>
2022-04-12 19:17:46 +0000

    Prova

--- src/Funcional/stream.js
+++ src/Funcional/stream.js
@@ -2,35 +2,19 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const stream = fs.createReadStream('./src/Funcional/sales.csv','utf-8');
 
-const headers = new Set();
-
-let 
-
 const streamCsv =  csv.parse({
                         headers:true,
+                        // skipLines: 2,
                         // delimiter:'\n',
-                        // quote:'"'
-                    })
-                    .transform(data => {
-                        headers.add(data.YEAR_ID);
+                        // quote:"'"
                     })
                     .on('error', error => console.error(error))
                     .on('data',data => {
-                        // console.log(data);
-                        // saveCsv(row,"sales_2003");
-                    })
-                    .on('end',rowCount=>console.log('Finished',rowCount));
+                        if(data.YEAR_ID != undefined){
+                            csv.write([data],{headers:true,skipRows: 2 })
+                            .on('error', error => console.error(error))
+                            .pipe(fs.createWriteStream(`./src/Funcional/${data.YEAR_ID}.csv`,'utf-8'));
+                        }
+                    });
 
 stream.pipe(streamCsv);
-
-const saveCsv = (data,name_file)=>{
-    console.log(data);
-    let ws = fs.createWriteStream(`./src/Funcional/${name_file}.csv`,'utf-8');
-    csv.write(data,{headers:true,})
-    .on('error', error => console.error(error))
-    .on('end',()=> console.log("Arquivo salvo!!!"))
-    .pipe(ws);
-}
-
-
-// Pegar o ano de cada dado e separar os dados do mesmo ano por grupo.
--- src/prova.js
+++ src/prova.js
@@ -1,17 +1,45 @@
-// Sabemos que atualmente diversas linguagens suportam este paradigma, em sua totalidade ou parcialmente.
 
+// Sabemos que atualmente diversas linguagens suportam este paradigma, em sua totalidade ou parcialmente.
 // Nesta questão você deverá colocar em prática seus conhecimentos para desenvolver uma função em JavaScript que substitua todas as vogais de uma string recebida por parâmetros pelo caracter _ (underline). Esta função deverá colocar em prática os conceitos de pureza, imutabilidade, etc. 
 
 const word = 'palavra';
 
-function setUnderLine([...word],index = word.length){
+function setUnderLine([...word],index=word.length){
     let i = index-1;
+    let regex = /[AEIOU-aeiou]/;
     if(i < 0){
         return word.join('');
-    }else if(word[i].toLowerCase() === 'a' || word[i].toLowerCase() === 'e' || word[i].toLowerCase() === 'i' ||word[i].toLowerCase() === 'o' ||word[i].toLowerCase() === 'u'){
+    }
+    if(regex.test(word[i])){
         word[i] = '_';
     }
     return setUnderLine(word,i);
 }
+// console.log(setUnderLine(word));
 
-console.log(setUnderLine(word))
+// Discultir em aula com professor
+function setUnderLinex([...word],index=word.length,regex=/[AEIOU-aeiou]/,shift='_'){
+    if(index-1 < 0) return word.join('');
+    else if(regex.test(word[index-1])) word[index-1] = shift;
+    return setUnderLine(word,index-1,regex,shift);
+}
+
+function removerVogal([letra,...word]){
+    // let regex = /[AEIOU-aeiou]/;
+    // if(letra == undefined) return '';
+    // else if(regex.test(letra)) return '_' + removerVogal(word);
+    // return letra + removerVogal(word);
+    return letra == undefined ? '' : (/[AEIOU-aeiou]/.test(letra) ? '_'+removerVogal(word) : letra+removerVogal(word));
+}
+
+const removerVogais = ([letra,...word]) => letra == undefined ? '' : (/[AEIOU-aeiou]/.test(letra) ? '_'+removerVogal(word) : letra+removerVogal(word));
+
+console.log(removerVogais('gilson santos'));
+
+// console.log(setUnderLinex(word));
+
+setUnderLinex(word);
+
+function fullname({name='Gilson',lastname='Santos'}={}){
+    console.log(name + lastname);
+}
